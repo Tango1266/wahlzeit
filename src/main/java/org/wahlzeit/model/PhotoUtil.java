f4eb726252f111f872831e200204e71890739bcb
tango1266 <tango1266@arcor.de>
2017-11-13 00:26:31 +0100

    Added Todo

--- src/main/java/org/wahlzeit/model/PhotoUtil.java
+++ src/main/java/org/wahlzeit/model/PhotoUtil.java
@@ -1,21 +1,25 @@
 /*
- * Copyright (c) 2006-2009 by Dirk Riehle, http://dirkriehle.com
+ *  Copyright
  *
- * This file is part of the Wahlzeit photo rating application.
+ *  Classname: PhotoUtil
+ *  Author: Tango1266
+ *  Version: 12.11.17 23:41
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ *  This file is part of the Wahlzeit photo rating application.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU Affero General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public
+ *  License along with this program. If not, see
+ *  <http://www.gnu.org/licenses/>
  */
 
 package org.wahlzeit.model;
@@ -52,6 +56,8 @@ public class PhotoUtil {
 
         return result;
     }
+
+    /*TODO: Check whether createGurkenPhoto is necessary (if so, then figure out how to handle many parameter)*/
 
     /**
      *
