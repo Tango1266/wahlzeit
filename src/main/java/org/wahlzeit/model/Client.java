206cc5d1a02b8f1f9c68c59637b8d886d7444202
tango1266 <tango1266@arcor.de>
2018-01-31 14:19:25 +0100

    SysConfig.setInstance suddenly threw an exception redoploy with this revert seemed to work. Revert "Fix NullPointerException - AbstractWebPartHandler - Client"
    This reverts commit df2771c

--- src/main/java/org/wahlzeit/model/Client.java
+++ src/main/java/org/wahlzeit/model/Client.java
@@ -1,25 +1,21 @@
 /*
- *  Copyright
+ * Copyright (c) 2006-2009 by Dirk Riehle, http://dirkriehle.com
  *
- *  Classname: Client
- *  Author: Tango1266
- *  Version: 08.11.17 22:26
+ * This file is part of the Wahlzeit photo rating application.
  *
- *  This file is part of the Wahlzeit photo rating application.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public
- *  License along with this program. If not, see
- *  <http://www.gnu.org/licenses/>
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 package org.wahlzeit.model;
@@ -45,293 +41,302 @@ import java.util.List;
 @Entity
 public abstract class Client implements Serializable, Persistent {
 
-    private String httpSessionId;
-    @Id
-    protected String id;
-    protected String nickName;
-    @Parent
-    protected Key parent = ObjectManager.applicationRootKey;
-    /**
-     *
-     */
-    protected EmailAddress emailAddress = EmailAddress.EMPTY;
-    /**
-     *
-     */
-    protected AccessRights accessRights = AccessRights.NONE;
-    @Ignore
-    protected int writeCount = 0;
-    protected Language language = Language.ENGLISH;
-    protected PhotoSize photoSize = PhotoSize.MEDIUM;
-    protected List<PhotoId> praisedPhotoIds = new ArrayList<>();
-    protected List<PhotoId> skippedPhotoIds = new ArrayList<>();
-    public static final String ID = "id";
-    public static final String NICK_NAME = "nickName";
-    public static final String LANGUAGE = "language";
+	public static final String ID = "id";
+	public static final String NICK_NAME = "nickName";
+	public static final String LANGUAGE = "language";
 
-    /**
-     *
-     */
-    Client() {
-        // do nothing
-    }
+	@Id
+	protected String id;
 
-    /**
-     * @methodtype get
-     */
-    public String getId() {
-        return id;
-    }
+	protected String nickName;
 
-    /**
-     * @methodtype get
-     */
-    public AccessRights getAccessRights() {
-        return accessRights;
-    }
+	@Parent
+	protected Key parent = ObjectManager.applicationRootKey;
 
-    /**
-     * @methodtype set
-     */
-    public void setAccessRights(AccessRights newRights) {
-        accessRights = newRights;
-        incWriteCount();
-    }
+	/**
+	 *
+	 */
+	protected EmailAddress emailAddress = EmailAddress.EMPTY;
 
-    /**
-     * @methodtype get
-     */
-    public String getNickName() {
-        return nickName;
-    }
+	/**
+	 *
+	 */
+	protected AccessRights accessRights = AccessRights.NONE;
 
-    /**
-     * @methodtype set
-     */
-    public void setNickName(String nickName) throws IllegalArgumentException {
-        UserManager.getInstance().changeNickname(this.nickName, nickName);
-        this.nickName = nickName;
-        incWriteCount();
-    }
+	@Ignore
+	protected int writeCount = 0;
 
-    /**
-     * @methodtype boolean-query
-     */
-    public boolean hasGuestRights() {
-        return hasRights(AccessRights.GUEST);
-    }
+	private String httpSessionId;
 
-    /**
-     * @methodtype boolean-query
-     */
-    public boolean hasRights(AccessRights otherRights) {
-        return AccessRights.hasRights(accessRights, otherRights);
-    }
+	protected Language language = Language.ENGLISH;
 
-    /**
-     *
-     */
-    public boolean hasUserRights() {
-        return hasRights(AccessRights.USER);
-    }
+	protected PhotoSize photoSize = PhotoSize.MEDIUM;
 
-    /**
-     * @methodtype boolean-query
-     */
-    public boolean hasModeratorRights() {
-        return hasRights(AccessRights.MODERATOR);
-    }
+	protected List<PhotoId> praisedPhotoIds = new ArrayList<PhotoId>();
 
-    /**
-     * @methodtype boolean-query
-     */
-    public boolean hasAdministratorRights() {
-        return hasRights(AccessRights.ADMINISTRATOR);
-    }
+	protected List<PhotoId> skippedPhotoIds = new ArrayList<PhotoId>();
 
-    /**
-     * @methodtype get
-     */
-    public EmailAddress getEmailAddress() {
-        return emailAddress;
-    }
 
-    /**
-     *
-     */
-    public void removeHttpSessionId() {
-        httpSessionId = null;
-        incWriteCount();
-    }
+	/**
+	 *
+	 */
+	Client() {
+		// do nothing
+	}
 
-    public String getHttpSessionId() {
-        return httpSessionId;
-    }
+	/**
+	 * @methodtype initialization
+	 */
+	protected void initialize(String id, String nickName, EmailAddress emailAddress, AccessRights accessRights,
+							  Client previousClient) {
+		this.id = id;
+		this.nickName = nickName;
+		this.accessRights = accessRights;
+		this.emailAddress = emailAddress;
 
-    /**
-     *
-     */
-    public void setHttpSessionId(String httpSessionId) {
-        this.httpSessionId = httpSessionId;
-        incWriteCount();
-    }
+		// use some of the existing properties for the new user
+		if (previousClient != null) {
+			this.setLanguage(previousClient.getLanguage());
+			this.setPraisedPhotoIds(previousClient.getPraisedPhotoIds());
+			this.setPhotoSize(previousClient.getPhotoSize());
+		}
 
-    /**
-     * @methodtype get
-     */
-    public Language getLanguage() {
-        return language;
-    }
+		incWriteCount();
 
-    /**
-     * @methodtype set
-     */
-    public void setLanguage(Language newLanguage) {
-        language = newLanguage;
-        incWriteCount();
-        doSetLanguage(newLanguage);
-    }
+		UserManager.getInstance().addClient(this);
+	}
 
-    /**
-     * @methodtype get
-     */
-    public ModelConfig getLanguageConfiguration() {
-        return LanguageConfigs.get(language);
-    }
+	/**
+	 * @methodtype get
+	 */
+	public String getId() {
+		return id;
+	}
 
-    /**
-     * @methodtype get
-     */
-    public PhotoSize getPhotoSize() {
-        return photoSize;
-    }
+	/**
+	 * @methodtype get
+	 */
+	public AccessRights getAccessRights() {
+		return accessRights;
+	}
 
-    /**
-     * @methodtype set
-     */
-    public void setPhotoSize(PhotoSize photoSize) {
-        this.photoSize = photoSize;
-    }
+	/**
+	 * @methodtype set
+	 */
+	public void setAccessRights(AccessRights newRights) {
+		accessRights = newRights;
+		incWriteCount();
+	}
 
-    /**
-     * @methodtype get
-     */
-    public List<PhotoId> getPraisedPhotoIds() {
-        return praisedPhotoIds;
-    }
+	/**
+	 * @methodtype get
+	 */
+	public String getNickName() {
+		return nickName;
+	}
 
-    /**
-     * @methodtype set
-     */
-    public void setPraisedPhotoIds(List<PhotoId> praisedPhotoIds) {
-        this.praisedPhotoIds = praisedPhotoIds;
-    }
+	/**
+	 * @methodtype set
+	 */
+	public void setNickName(String nickName) throws IllegalArgumentException {
+		UserManager.getInstance().changeNickname(this.nickName, nickName);
+		this.nickName = nickName;
+		incWriteCount();
+	}
 
-    /**
-     * @methodtype set
-     */
-    public void addPraisedPhotoId(PhotoId ratedPhotoId) {
-        praisedPhotoIds.add(ratedPhotoId);
-        removeSkippedPhotoId(ratedPhotoId);
-    }
+	/**
+	 * @methodtype boolean-query
+	 */
+	public boolean hasGuestRights() {
+		return hasRights(AccessRights.GUEST);
+	}
 
-    /**
-     * @methodtype get
-     */
-    public Photo getLastPraisedPhoto() {
-        int indexOfLastPraisedPhoto = praisedPhotoIds.size() - 1;
-        Photo result = null;
-        while (indexOfLastPraisedPhoto >= 0 && result == null) {
-            PhotoId lastPraisedPhotoId = praisedPhotoIds.get(indexOfLastPraisedPhoto);
-            result = PhotoManager.getInstance().getPhoto(lastPraisedPhotoId);
-            if (!result.isVisible()) {
-                result = null;
-                indexOfLastPraisedPhoto--;
-            }
-        }
-        return result;
-    }
+	/**
+	 * @methodtype boolean-query
+	 */
+	public boolean hasRights(AccessRights otherRights) {
+		return AccessRights.hasRights(accessRights, otherRights);
+	}
 
-    /**
-     * @methodtype get
-     */
-    public List<PhotoId> getSkippedPhotoIds() {
-        return skippedPhotoIds;
-    }
+	/**
+	 *
+	 */
+	public boolean hasUserRights() {
+		return hasRights(AccessRights.USER);
+	}
 
-    /**
-     * @methodtype get
-     */
-    public void setSkippedPhotoIds(List<PhotoId> skippedPhotoIds) {
-        this.skippedPhotoIds = skippedPhotoIds;
-    }
+	/**
+	 * @methodtype boolean-query
+	 */
+	public boolean hasModeratorRights() {
+		return hasRights(AccessRights.MODERATOR);
+	}
 
-    /**
-     * @methodtype set
-     */
-    public void removeSkippedPhotoId(PhotoId skippedPhotoIdToRemove) {
-        skippedPhotoIds.remove(skippedPhotoIdToRemove);
-    }
+	/**
+	 * @methodtype boolean-query
+	 */
+	public boolean hasAdministratorRights() {
+		return hasRights(AccessRights.ADMINISTRATOR);
+	}
 
-    /**
-     * @methodtype set
-     */
-    public void addSkippedPhotoId(PhotoId skippedPhotoId) {
-        if (!skippedPhotoIds.contains(skippedPhotoId)) {
-            skippedPhotoIds.add(skippedPhotoId);
-        }
-    }
+	/**
+	 * @methodtype get
+	 */
+	public EmailAddress getEmailAddress() {
+		return emailAddress;
+	}
 
-    /**
-     * @methodtype initialization
-     */
-    protected void initialize(String id, String nickName, EmailAddress emailAddress, AccessRights accessRights,
-                              Client previousClient) {
-        this.id = id;
-        this.nickName = nickName;
-        this.accessRights = accessRights;
-        this.emailAddress = emailAddress;
+	/**
+	 *
+	 */
+	public boolean isDirty() {
+		return writeCount != 0;
+	}
 
-        // use some of the existing properties for the new user
-        if (previousClient != null) {
-            setLanguage(previousClient.getLanguage());
-            setPraisedPhotoIds(previousClient.getPraisedPhotoIds());
-            setPhotoSize(previousClient.getPhotoSize());
-        }
+	/**
+	 *
+	 */
+	public void incWriteCount() {
+		writeCount++;
+	}
 
-        incWriteCount();
+	/**
+	 *
+	 */
+	public void resetWriteCount() {
+		writeCount = 0;
+	}
 
-        UserManager.getInstance().addClient(this);
-    }
+	/**
+	 *
+	 */
+	public void removeHttpSessionId() {
+		httpSessionId = null;
+		incWriteCount();
+	}
 
-    /**
-     * @methodtype set
-     * @methodpoperty hook
-     */
-    protected void doSetLanguage(Language newLanguage) {
-    }
+	public String getHttpSessionId() {
+		return httpSessionId;
+	}
 
-    /**
-     *
-     */
-    @Override
-    public boolean isDirty() {
-        return writeCount != 0;
-    }
+	/**
+	 *
+	 */
+	public void setHttpSessionId(String httpSessionId) {
+		this.httpSessionId = httpSessionId;
+		incWriteCount();
+	}
 
-    /**
-     *
-     */
-    @Override
-    public void incWriteCount() {
-        writeCount++;
-    }
+	/**
+	 * @methodtype get
+	 */
+	public Language getLanguage() {
+		return language;
+	}
 
-    /**
-     *
-     */
-    @Override
-    public void resetWriteCount() {
-        writeCount = 0;
-    }
+	/**
+	 * @methodtype set
+	 */
+	public void setLanguage(Language newLanguage) {
+		language = newLanguage;
+		incWriteCount();
+		doSetLanguage(newLanguage);
+	}
+
+	/**
+	 * @methodtype set
+	 * @methodpoperty hook
+	 */
+	protected void doSetLanguage(Language newLanguage) {
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	public ModelConfig getLanguageConfiguration() {
+		return LanguageConfigs.get(language);
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	public PhotoSize getPhotoSize() {
+		return photoSize;
+	}
+
+	/**
+	 * @methodtype set
+	 */
+	public void setPhotoSize(PhotoSize photoSize) {
+		this.photoSize = photoSize;
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	public List<PhotoId> getPraisedPhotoIds() {
+		return praisedPhotoIds;
+	}
+
+	/**
+	 * @methodtype set
+	 */
+	public void setPraisedPhotoIds(List<PhotoId> praisedPhotoIds) {
+		this.praisedPhotoIds = praisedPhotoIds;
+	}
+
+	/**
+	 * @methodtype set
+	 */
+	public void addPraisedPhotoId(PhotoId ratedPhotoId) {
+		praisedPhotoIds.add(ratedPhotoId);
+		removeSkippedPhotoId(ratedPhotoId);
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	public Photo getLastPraisedPhoto() {
+		int indexOfLastPraisedPhoto = praisedPhotoIds.size() - 1;
+		Photo result = null;
+		while (indexOfLastPraisedPhoto >= 0 && result == null) {
+			PhotoId lastPraisedPhotoId = praisedPhotoIds.get(indexOfLastPraisedPhoto);
+			result = PhotoManager.getInstance().getPhoto(lastPraisedPhotoId);
+			if (!result.isVisible()) {
+				result = null;
+				indexOfLastPraisedPhoto--;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	public List<PhotoId> getSkippedPhotoIds() {
+		return skippedPhotoIds;
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	public void setSkippedPhotoIds(List<PhotoId> skippedPhotoIds) {
+		this.skippedPhotoIds = skippedPhotoIds;
+	}
+
+	/**
+	 * @methodtype set
+	 */
+	public void removeSkippedPhotoId(PhotoId skippedPhotoIdToRemove) {
+		skippedPhotoIds.remove(skippedPhotoIdToRemove);
+	}
+
+	/**
+	 * @methodtype set
+	 */
+	public void addSkippedPhotoId(PhotoId skippedPhotoId) {
+		if (!skippedPhotoIds.contains(skippedPhotoId)) {
+			skippedPhotoIds.add(skippedPhotoId);
+		}
+	}
 }
